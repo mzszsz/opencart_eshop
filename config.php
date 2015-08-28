b71d064e31a8db1a1ea3210006d673c9b391df38
mzszsz <manishaggarwal@mastersoftwaresolutions.com>
2015-08-28 17:08:54 +0530

    config changed
    new details added

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ define('DIR_LOGS', '/var/www/html/eshoplift_oc/system/logs/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'root');   
 define('DB_DATABASE', 'eshopliftoc');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
